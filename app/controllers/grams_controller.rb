41395f9495655782b04cb0702e8e19650e74dc66
Samy Ayoub <samyayoub@gmail.com>
2019-06-08 17:00:14 +0000

    Editing and Updating Grams is working; Destroying Grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create, :edit]
+    before_action :authenticate_user!, only: [:new, :create]
 
     def index
 
@@ -26,9 +26,29 @@ class GramsController < ApplicationController
     def edit
         @gram = Gram.find_by_id(params[:id])
         return render_not_found if @gram.blank?
-        # if @gram.blank?
-        #     render plain: 'Not Found', status: :not_found
-        # end
+        if @gram.blank?
+            render plain: 'Not Found', status: :not_found
+        end
+    end
+
+    def update
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+
+        @gram.update_attributes(gram_params)
+        
+        if @gram.valid?
+            redirect_to root_path
+        else
+            return render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+        @gram.destroy
+        redirect_to root_path
     end
 
     private
