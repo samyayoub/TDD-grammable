dba162c211c4cb65c811a6e07435a71125e8762c
Samy Ayoub <samyayoub@gmail.com>
2019-06-09 00:06:06 +0000

    Working on Image Uploader/Carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,9 @@
 CarrierWave.configure do |config|
-  if Rails.env.development?
-    config.cache_dir = '/home/vagrant/uploads_tmp/tmp/uploads'
-    config.root = '/home/vagrant/uploads_tmp/tmp'
-  end
-
-  config.storage    = :aws
-  config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl    = "public-read"
-
-  config.aws_credentials = {
-      access_key_id:     ENV["AWS_ACCESS_KEY"],
-      secret_access_key: ENV["AWS_SECRET_KEY"],
-      region:            ENV["AWS_REGION"]
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider:               'AWS',
+    aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
+    aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
   }
+  config.fog_directory =    ENV["AWS_BUCKET"]
 end
